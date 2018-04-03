6388aa58fb9fed020a1fee2834ba61ea5660b4c0
Fachran Dhani <fachran.dhani@ralali.com>
2018-04-03 22:16:56 +0700

    initialize

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.admin={};
+
+config.app={};
+
+config.admin.name="Fachran Dhani Achmad";
+config.admin.email="fachran.dhani@gmail.com";
+config.app.name="Papantulis";
+config.app.url="";
+
+module.exports=config
